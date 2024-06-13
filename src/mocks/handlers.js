fe2d5d6f056959b01f523616558048f80fff5a26
Alexander Ilseman <al@ilseman.com>
2024-06-13 16:13:54 -0500

    Fixed mocked url

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("/api/v1/todos", () => {
+  http.get("http://localhost:3300/api/v1/todos", () => {
     return HttpResponse.json([
       {
         id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",

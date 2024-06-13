d9d990c3639ab9d766c659344d2805140727eb63
Alexander Ilseman <al@ilseman.com>
2024-06-13 15:36:46 -0500

    Completed mocking setup (maybe)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+ 
+enableMocking().then(() => {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
@@ -12,6 +25,8 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,11 +7,13 @@ export const handlers = [
         id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
         name: "Delete code",
         body: "Very bad code must go.",
+        tags: [],
       },
       {
         id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
         title: "Write code",
         body: "All of the code.",
+        tags: [],
       },
     ]);
   }),
